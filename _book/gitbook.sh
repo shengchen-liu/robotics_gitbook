ceb8107b649b0ea54e2c31a308c527e1a7c8748d
shengchen-liu <shengchen.liu1225@gmail.com>
2020-12-29 22:58:18 -0500

    math

---
+++ _book/gitbook.sh
@@ -0,0 +1,12 @@
+gitbook build
+git checkout master
+git add .
+git commit -m $1
+git push -u origin master
+git checkout gh-pages
+git checkout master .  #copy the contents into gh-pages
+cp -r _book/* .
+git add .
+git commit -m $1
+git push -u origin gh-pages
+git checkout master
